2ac60e3057f5a72cdf1a64a23f45c32ddcaf4c11
jaemyung.kim <jaemyung.kim@navercorp.com>
2020-06-08 16:32:25 +0900

    runtime test

--- src/main/scala/com/github/windbird123/overview/TestEffect.scala
+++ src/main/scala/com/github/windbird123/overview/TestEffect.scala
@@ -16,17 +16,25 @@ object TestEffect {
     // simple case
     val eff = for {
       server <- ZIO.access[Config](_.server)
-      port <- ZIO.access[Config](_.port)
+      port   <- ZIO.access[Config](_.port)
     } yield s"server: $server, port: $port"
 
     val ready = eff.provide(config)
     Runtime.default.unsafeRunSync(ready)
 
     // config 를 가져오기 위해 계산이 필요할 경우 (environmental effects)
-    val tablesAndColumns
-      : ZIO[DatabaseOps, Throwable, (List[String], List[String])] = for {
-      tables <- ZIO.accessM[DatabaseOps](_.getTableNames)
+    val tablesAndColumns: ZIO[DatabaseOps, Throwable, (List[String], List[String])] = for {
+      tables  <- ZIO.accessM[DatabaseOps](_.getTableNames)
       columns <- ZIO.accessM[DatabaseOps](_.getColumnNames("user_table"))
     } yield (tables, columns)
+  }
+}
+
+object RunTimeTest {
+  def main(args: Array[String]): Unit = {
+    val list = 1 to 10
+    import zio.duration._
+    val eff = ZIO.foreachParN(2)(list)(x => UIO(println(s"NUM: $x")).delay(1.second))
+    zio.Runtime.default.unsafeRun(eff)
   }
 }
