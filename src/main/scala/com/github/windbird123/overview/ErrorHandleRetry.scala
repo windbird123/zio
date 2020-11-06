189a92abaff9330039ca07e087c18b5b102b1568
jaemyung.kim <jaemyung.kim@navercorp.com>
2020-11-06 10:31:54 +0900

    error handling

--- src/main/scala/com/github/windbird123/overview/ErrorHandleRetry.scala
+++ src/main/scala/com/github/windbird123/overview/ErrorHandleRetry.scala
@@ -14,10 +14,15 @@ object ErrorHandleRetry extends App {
 
 
     trait DBError
+    case object RecordNotFound extends DBError
     val lookupDB : IO[DBError, Option[String]] = ???
 
     // lookupDB 의 결과가 None 일 경우, 이를 Error 쪽에 포함되도록 한다.
     val dbSome: ZIO[Any, Option[DBError], String] = lookupDB.some
+    val dbSomeError: ZIO[Any, DBError, String] = dbSome.mapError {
+      case Some(x) => x
+      case None => RecordNotFound
+    }
 
     ///////////////////////////////////////////////////////////////////////
     // tapError 와 orElse 를 적극적으로 사용하자. 끝판왕은 foldM
