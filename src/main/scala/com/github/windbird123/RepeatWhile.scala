8f9d7e619bd3b441fd341dd4a707421ee568853a
jaemyung.kim <jaemyung.kim@navercorp.com>
2020-11-22 14:44:20 +0900

    repeat while

--- src/main/scala/com/github/windbird123/RepeatWhile.scala
+++ src/main/scala/com/github/windbird123/RepeatWhile.scala
@@ -1,6 +1,7 @@
 package com.github.windbird123
 
 import zio._
+import zio.duration._
 
 object RepeatWhile extends zio.App {
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
@@ -9,6 +10,9 @@ object RepeatWhile extends zio.App {
       _ <- console.putStrLn(s"Called: $r")
     } yield r
 
-    effect.repeatWhile(_ < 0.9).exitCode
+    effect.repeatWhileM { x =>
+      if (x < 0.9) UIO(println("repeat ..")) *> UIO(true).delay(1.second)
+      else UIO(false)
+    }.exitCode
   }
 }
