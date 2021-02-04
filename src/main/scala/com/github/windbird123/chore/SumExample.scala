807314610784e2b742897254345eecde9b82719d
jaemyung.kim <jaemyung.kim@navercorp.com>
2021-02-04 10:34:58 +0900

    comment

--- src/main/scala/com/github/windbird123/chore/SumExample.scala
+++ src/main/scala/com/github/windbird123/chore/SumExample.scala
@@ -14,3 +14,9 @@ object SumExample {
     case x :: tail => x + functionalSum(tail)
   }
 }
+
+// 1. What's wrong imperativeSum? using var ?
+// 2. functionalSum blow stack ?
+// 3. which is faster ?
+// 4. which approach more maintainable ?
+// 5. if I need parallel version of sum, which on is better ?
